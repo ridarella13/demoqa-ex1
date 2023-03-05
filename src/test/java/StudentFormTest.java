3bb5fc18ef7ef8c3dc2a416947cb5d43489460b4
Tati <ridarella13@gmail.com>
2023-03-05 18:03:11 +0300

    added changes

--- src/test/java/StudentFormTest.java
+++ src/test/java/StudentFormTest.java
@@ -1,8 +1,11 @@
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import pages.StudentPage;
+
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
@@ -13,6 +16,10 @@ public class StudentFormTest {
     @BeforeAll
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
+    }
+    @BeforeEach
+    void addListener() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
     }
 
     @Test
